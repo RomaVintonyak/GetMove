746a9d3c305f2a4afdc5d39aef40a906e60d4b65
RomaVintonyak <19roman23@gmail.com>
2020-11-03 14:40:19 +0200

    finished works section

--- js_src/main.js
+++ js_src/main.js
@@ -7,8 +7,7 @@ jQuery(document).ready(function () {
     menuBtn.not("active").removeClass("active__link");
     $(this).toggleClass("active__link");
   });
-  menuBtn.on("click", function (event) {
-    event.preventDefault();
+  menuBtn.on("click", function () {
     var link = $(this).data("link");
     $("[data-section]").each(function () {
       var section = $(this).data("section");
@@ -54,7 +53,91 @@ jQuery(document).ready(function () {
       },
     ],
   });
+  $("#sliderBrand").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    cssEase: "ease-in-out",
+    speed: 500,
+    swipe: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    /*responsive*/
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          fade: true,
+        },
+      },
+    ],
+  });
+  $("#sliderMobile").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    cssEase: "ease-in-out",
+    speed: 500,
+    swipe: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    /*responsive*/
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          fade: true,
+        },
+      },
+    ],
+  });
   /*work page change category project*/
+  var worksBtn = $("[data-works]");
+  worksBtn.on("click", function (event) {
+    event.preventDefault();
+    worksBtn.not("active").removeClass("btn__active");
+    $(this).toggleClass("btn__active");
+  });
+  worksBtn.on("click", function () {
+    var worksData = $(this).data("works");
+    $("[data-cat]").each(function () {
+      var catData = $(this).data("cat");
+      if (worksData == catData) {
+        $(this).addClass("show__category");
+      } else {
+        $(this).removeClass("show__category");
+      }
+    });
+  });
   
-
 });
